eee03f98f319844ea64bc528006aafb4d5e1510e
Innocent <99240481+Asabuodu@users.noreply.github.com>
2025-06-25 17:45:27 +0100

    token workin

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,21 +1,23 @@
-// src/utils/sendEmail.ts
+
+
+
 import nodemailer from "nodemailer";
 
-export default async function sendEmail({ to, subject, text }: { to: string, subject: string, text: string }) {
-  const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-      user: process.env.EMAIL_USER!,
-      pass: process.env.EMAIL_PASS!,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
+const sendEmail = async ({ to, subject, text }: { to: string; subject: string; text: string }) => {
   await transporter.sendMail({
     from: `"Timer App" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     text,
   });
-    console.log(`Email sent to ${to} with subject "${subject}"`);
-}
+};
 
+export default sendEmail;
