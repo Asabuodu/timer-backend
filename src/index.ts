b40b99de8a959b51c165f71ce1472109cc4ff0b9
Innocent <99240481+Asabuodu@users.noreply.github.com>
2025-06-30 15:16:03 +0100

    build successful

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,8 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:3000",
+  // origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "https://timer-frontend-iota.vercel.app",
   credentials: true,
 }));
 
